9ae7b86fdf0a2340e78af56d8425e8ed15d79438
Hannes Kristján Hannesson <hanneskr4@gmail.com>
2018-05-29 23:30:00 +0000

    markers are loaded, but still buggy

--- app/src/components/Map/index.jsx
+++ app/src/components/Map/index.jsx
@@ -1,18 +1,89 @@
 import React, { Component } from 'react';
 import withScriptJs from 'react-google-maps/lib/withScriptjs';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import CustomMarker from '../CustomMarker';
+import { getEntries } from '../../services/markerService';
+import { compose, withProps, withStateHandlers } from "recompose";
 
 
-const MapView = withScriptJs(withGoogleMap(props =>
+var a = getEntries().map((marker) => <Marker position={{lat: marker.lat, lng: marker.lng}} />)
+console.log(a);
+
+function aggregateMarkers() {
+  var markerObjects = [];
+  var markers = getEntries();
+  for (var key in markers) {
+    markerObjects.push(<CustomMarker key={key}
+                               user={markers[key].user}
+                               photoMarker={markers[key].photoMarker}
+                               lat={markers[key].lat}
+                               lng={markers[key].lng}
+                               post={markers[key].post}
+                               position={{lat: markers[key].lat, lng: markers[key].lng}}/>);
+  }
+
+  console.log(markerObjects);
+  return markerObjects;
+
+}
+
+
+const MapView = compose(
+  withStateHandlers(() => ({
+    isOpen: false,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+  }),
+  withScriptJs,
+  withGoogleMap
+)(props =>
   <GoogleMap
     defaultZoom={8}
     defaultCenter={{ lat: -34.397, lng: 150.644 }}
   >
+  {getEntries().map((marker) => 
+  
     <Marker
-      position={{ lat: -34.397, lng: 150.644 }}
-    />
+      position={{ lat: marker.lat, lng: marker.lng }}
+      onClick={props.onToggleOpen}
+    >
+      {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+      <CustomMarker
+                            user={marker.user}
+                            post={marker.post}
+                            photoMarker={false}
+                            key={marker.post}
+                          />
+      </InfoWindow>
+      }
+    </Marker>
+  
+  )}
   </GoogleMap>
-));
+);
+
+
+/*const MapView = withScriptJs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+
+   {getEntries().map((marker) => 
+      <Marker position={{lat: marker.lat, lng: marker.lng}}>
+        {props.isOpen && <CustomMarker
+                            user={marker.post}
+                            post={marker.post}
+                            photoMarker={false}
+                            key={marker.post}
+                            position={{lat: marker.lat, lng: marker.lng}}
+                          />
+        }
+      </Marker>)}
+  </GoogleMap>
+));*/
 
 export default MapView;
 
